7d268713d44bb2002ae19ee6004f2cd6afb245a8
chexq <chexq35@gmail.com>
2024-01-01 11:21:42 -0500

    enum Rank and enum Suit done

---
+++ Black Jack.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+enum Rank
+{
+    rank_ace,
+    rank_2,
+    rank_3,
+    rank_4,
+    rank_5,
+    rank_6,
+    rank_7,
+    rank_8,
+    rank_9,
+    rank_10,
+    rank_jack,
+    rank_queen,
+    rank_king,
+    max_ranks,
+};
+
+enum Suit
+{
+    suit_club,
+    suit_diamond,
+    suit_heart,
+    suit_spade,
+    max_suits,
+};
+
+int main()
+{
+  
+    return 0;
+}
