8a4d3cc66a82089bd9634d829f6c278a5152713a
chexq <chexq35@gmail.com>
2024-01-01 12:04:46 -0500

    Deck default constructor done

--- Black Jack.cpp
+++ Black Jack.cpp
@@ -3,65 +3,69 @@
 
 struct Card
 {
-    enum Rank
-    {
-        rank_ace,
-        rank_2,
-        rank_3,
-        rank_4,
-        rank_5,
-        rank_6,
-        rank_7,
-        rank_8,
-        rank_9,
-        rank_10,
-        rank_jack,
-        rank_queen,
-        rank_king,
-        max_ranks,
-    };
+	enum Rank
+	{
+		rank_ace,
+		rank_2,
+		rank_3,
+		rank_4,
+		rank_5,
+		rank_6,
+		rank_7,
+		rank_8,
+		rank_9,
+		rank_10,
+		rank_jack,
+		rank_queen,
+		rank_king,
+		max_ranks,
+	};
 
-    enum Suit
-    {
-        suit_club,
-        suit_diamond,
-        suit_heart,
-        suit_spade,
-        max_suits,
-    };
+	enum Suit
+	{
+		suit_club,
+		suit_diamond,
+		suit_heart,
+		suit_spade,
+		max_suits,
+	};
 
-    Rank rank{};
-    Suit suit{};
-    static constexpr std::array allRanks{ rank_ace, rank_2, rank_3, rank_4, rank_5, rank_6,
-        rank_7, rank_8, rank_9, rank_10, rank_jack, rank_queen, rank_king };
-    static constexpr std::array allSuits{ suit_club, suit_diamond, suit_heart, suit_spade };
+	Rank rank{};
+	Suit suit{};
+	static constexpr std::array allRanks{ rank_ace, rank_2, rank_3, rank_4, rank_5, rank_6,
+		rank_7, rank_8, rank_9, rank_10, rank_jack, rank_queen, rank_king };
+	static constexpr std::array allSuits{ suit_club, suit_diamond, suit_heart, suit_spade };
 
-    int value() const
-    {
-        static constexpr std::array RankValues{ 11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10 };
-        return RankValues[rank];
-    }
+	int value() const
+	{
+		static constexpr std::array RankValues{ 11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10 };
+		return RankValues[rank];
+	}
 
-    friend std::ostream& operator<<(std::ostream& out, const Card& card)
-    {
-        static constexpr std::array ranks{ 'A', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K' };
-        static constexpr std::array suits{ 'C', 'D', 'H', 'S' };
-        out << ranks[card.rank] << suits[card.suit];
-        return out;
-    }
+	friend std::ostream& operator<<(std::ostream& out, const Card& card)
+	{
+		static constexpr std::array ranks{ 'A', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K' };
+		static constexpr std::array suits{ 'C', 'D', 'H', 'S' };
+		out << ranks[card.rank] << suits[card.suit];
+		return out;
+	}
+};
+
+class Deck
+{
+public:
+	Deck()
+	{
+		std::size_t count{};
+		for (auto suit : Card::allSuits)
+			for (auto rank : Card::allRanks)
+				m_cards[count++] = Card{ rank, suit };
+	}
+private:
+	std::array<Card, 52> m_cards{};
 };
 
 int main()
 {
-    // Print one card
-    Card card{ Card::rank_5, Card::suit_heart };
-    std::cout << card << '\n';
-
-    // Print all cards
-    for (auto suit : Card::allSuits)
-        for (auto rank : Card::allRanks)
-            std::cout << Card{ rank, suit } << ' ';
-    std::cout << '\n';
-
-    return 0;
+	return 0;
 }
